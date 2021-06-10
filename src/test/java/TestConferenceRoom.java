2a3c3775297e94d7f87a99624c666d5eb4eca983
MistaRae <itsamistarae@gmail.com>
2021-06-10 12:17:15 +0100

    conference room tests passing

---
+++ src/test/java/TestConferenceRoom.java
@@ -0,0 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestConferenceRoom {
+
+    ConferenceRoom conferenceRoom;
+    Guest mark;
+    ArrayList<Guest> occupants;
+
+    @Before
+    public void setUp(){
+        mark = new Guest("Mark");
+        conferenceRoom = new ConferenceRoom(occupants,10, "Business Suite" );
+    }
+
+    @Test
+    public void canGetOccupants(){
+        assertEquals(0, conferenceRoom.getNumOfOccupants());
+    }
+
+    @Test
+    public void canGetRoomCapacity(){
+        assertEquals(10,conferenceRoom.getCapacity());
+    }
+
+
+
+
+
+}
